994e482098aaf40a8bca5366c6787c3311937a10
jeantroiani <digitalguest@gmail.com>
2014-08-19 17:05:57 +0100

    tags as links are showing

--- spec/features/tag_feature_spec.rb
+++ spec/features/tag_feature_spec.rb
@@ -12,4 +12,15 @@ describe 'Tags' do
 		expect(page).to have_content('#weekend')
 		expect(page).to have_content('#party')
 	end
+
+	it 'redirects to show all the post with the same tag selected' do
+		post_1= Post.create(title:'Brighton', tags_list:'#food'  )
+		post_2=	Post.create(title:'London', 	tags_list:'#food'	)
+		post_3=	Post.create(title:'Caracas', 	tags_list:'#arepas')
+		visit('/posts')
+		click_link('#Brighton')
+		expect(page).to_not have_content('#arepas')
+		expect(page).to_not have_content('#food').exactly(2).times
+	end
+
 end
