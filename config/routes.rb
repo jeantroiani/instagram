bad230b013c718aa871fe06990865c74f099b881
jeantroiani <digitalguest@gmail.com>
2014-08-21 14:00:55 +0100

    Sign in and Sign out are working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :tags
   resources :posts
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,5 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
  
- let (:user) {User.create(user: 'jean', password: '12345678', password_confirmation: '12345678')}
 
- 	describe'user' do
- 	# 	context'sign up' do
- 	# 	it 'cannot post without signing up' do
- 	# 		visit('/posts')
- 	# 		click_link(Sign up)
- 	# 		fill_in 'email', 									with: 'test@mail.com' 
- 	# 		fill_in 'password', 							with: '12345678'
- 	# 		fill_in 'password confirmation', 	with: '12345678'
- 	# 		click_button 'Sing up'
- 	# 	end 
- 	# end
- end
 end
